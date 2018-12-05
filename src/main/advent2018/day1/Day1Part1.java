5ef9b0a4ccdd838ca8fdf1ef2a81cbfde3a1dc48
Aldrin Misquitta <aldrinnm@gmail.com>
2018-12-05 17:31:07 +0400

    Day 1 part 2 solution

--- src/main/advent2018/day1/Day1Part1.java
+++ src/main/advent2018/day1/Day1Part1.java
@@ -6,7 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class Day1 {
+public class Day1Part1 {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
 
@@ -16,10 +16,7 @@ public class Day1 {
 
         Path inputPath = Paths.get(ClassLoader.getSystemResource("day1.txt").toURI());
         Files.lines(inputPath)
-              .forEach(l -> {
-                  System.out.println("l = " + l);
-                  ref.freq += Long.parseLong(l);
-              });
+              .forEach(l -> ref.freq += Long.parseLong(l));
 
         System.out.println("ref.freq = " + ref.freq);
     }
